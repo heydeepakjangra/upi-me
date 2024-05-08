2a0434a8393099fe1eb058f6f7d917ee7ac9f73f
heydeepakjangra <heydeepakjangra@gmail.com>
2024-05-08 13:58:26 +0530

    quick fix

--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -5,11 +5,6 @@ import { useSearchParams } from "next/navigation";
 import QRCodeGenerator from "./_components/qrcode-generator";
 
 export default function Pay() {
-  const searchParams = useSearchParams();
-
-  const upi = searchParams.get("pa");
-  const amount = searchParams.get("am");
-  const note = searchParams.get("tn");
   return (
     <Suspense
       fallback={
@@ -18,9 +13,21 @@ export default function Pay() {
         </div>
       }
     >
-      <div className="gap-y-6 p-6 flex flex-col items-center justify-center h-screen bg-zinc-200">
-        <QRCodeGenerator upi={upi} amount={amount} note={note} />
-      </div>
+      <PayContent />
     </Suspense>
+  );
+}
+
+function PayContent() {
+  const searchParams = useSearchParams();
+
+  const upi = searchParams.get("pa");
+  const amount = searchParams.get("am");
+  const note = searchParams.get("tn");
+
+  return (
+    <div className="gap-y-6 p-6 flex flex-col items-center justify-center h-screen bg-zinc-200">
+      <QRCodeGenerator upi={upi} amount={amount} note={note} />
+    </div>
   );
 }
